be3ecd82550bf77f31515975c45b8e505d36fd84
Cardidi <cardidi@live.cn>
2023-03-20 09:09:58 +0800

    Initial Commit

---
+++ app/src/main/java/xyz/ca2didi/androidtv/am3party/DetailsDescriptionPresenter.kt
@@ -0,0 +1,16 @@
+package xyz.ca2didi.androidtv.am3party
+
+import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
+
+class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
+
+    override fun onBindDescription(
+            viewHolder: AbstractDetailsDescriptionPresenter.ViewHolder,
+            item: Any) {
+        val movie = item as Movie
+
+        viewHolder.title.text = movie.title
+        viewHolder.subtitle.text = movie.studio
+        viewHolder.body.text = movie.description
+    }
+}
